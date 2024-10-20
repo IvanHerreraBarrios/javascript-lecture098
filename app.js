8f77f0829c4c1276be728617ceb0c5ba8e3abb5a
Ivan Herrera Barrios <ivanherrerabarrios16@gmail.com>
2024-10-19 21:39:36 -0600

    lecture98

---
+++ app.js
@@ -0,0 +1,21 @@
+var deleteMiddle = function (head) {
+    const length = calculateLength(head)
+    if (length === 1) return null
+    const mid = Math.floor(length / 2)
+    let node = head
+    for (let i = 0; i < mid - 1; i++) {
+        node = node.next
+    }
+    node.next = node.next.next
+    return head
+};
+
+function calculateLength(head) {
+    let node = head
+    let pos = 0
+    while (node !== null) {
+        node = node.next
+        pos++
+    }
+    return pos
+}
